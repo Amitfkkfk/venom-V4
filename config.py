128b57476dc25c5b3c238379b97dc5bc472df3ab
Amitfkkfk <jilaal041@gmail.com>
2024-08-01 16:35:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7424479555:AAG21En_vWhghzaUAhKjJ-DoZaLV3KBH4")
+BOT_TOKEN=("7321871907:AAH6MVvvLWO2ldQEh7VKsHgkJFzFWsXPHF4")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("@Romeo804")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("6294789282")
 
 
 
